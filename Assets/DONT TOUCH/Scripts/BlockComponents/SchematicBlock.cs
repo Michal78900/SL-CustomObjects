b729c7e23e512a834178a4923c8aa63532ed4b83
Michal78900 <Michal78900@users.noreply.github.com>
2023-04-11 22:24:09 +0200

    The Funny

---
+++ Assets/DONT TOUCH/Scripts/BlockComponents/SchematicBlock.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public abstract class SchematicBlock : MonoBehaviour
+{
+    public abstract BlockType BlockType { get; }
+
+    public virtual bool Compile(SchematicBlockData block, Schematic schematic) => true;
+}
