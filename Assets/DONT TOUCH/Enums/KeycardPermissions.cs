d1c00ddc91aa9debf7c56092fee94c1abfbef5b4
Michal78900 <Michal78900@users.noreply.github.com>
2023-05-12 00:21:42 +0200

    Updated enums

--- Assets/DONT TOUCH/Enums/KeycardPermissions.cs
+++ Assets/DONT TOUCH/Enums/KeycardPermissions.cs
@@ -1,27 +1,63 @@
 ﻿[System.Flags]
-public enum KeycardPermissions : ushort
+public enum KeycardPermissions
 {
+    /// <summary>
+    /// No permissions.
+    /// </summary>
     None = 0,
-    
+
+    /// <summary>
+    /// Opens checkpoints.
+    /// </summary>
     Checkpoints = 1,
-    
+
+    /// <summary>
+    /// Opens <see cref="DoorType.GateA">Gate A</see> and <see cref="DoorType.GateB">Gate B</see>.
+    /// </summary>
     ExitGates = 2,
-    
+
+    /// <summary>
+    /// Opens <see cref="DoorType.Intercom">the Intercom door</see>.
+    /// </summary>
     Intercom = 4,
-    
+
+    /// <summary>
+    /// Opens the Alpha Warhead detonation room on surface.
+    /// </summary>
     AlphaWarhead = 8,
-    
-    ContainmentLevelOne = 16,
-    
-    ContainmentLevelTwo = 32,
-    
-    ContainmentLevelThree = 64,
-    
-    ArmoryLevelOne = 128,
-    
-    ArmoryLevelTwo = 256,
-    
-    ArmoryLevelThree = 512,
-    
-    ScpOverride = 1024,
+
+    /// <summary>
+    /// Opens <see cref="DoorType.Scp914Gate"/>.
+    /// </summary>
+    ContainmentLevelOne = 16, // 0x0010
+
+    /// <summary>
+    /// <see cref="ContainmentLevelOne"/>, <see cref="Checkpoints"/>.
+    /// </summary>
+    ContainmentLevelTwo = 32, // 0x0020
+
+    /// <summary>
+    /// <see cref="ContainmentLevelTwo"/>, <see cref="Intercom"/>, <see cref="AlphaWarhead"/>.
+    /// </summary>
+    ContainmentLevelThree = 64, // 0x0040
+
+    /// <summary>
+    /// <see cref="Checkpoints"/>, Opens Light Containment armory.
+    /// </summary>
+    ArmoryLevelOne = 128, // 0x0080
+
+    /// <summary>
+    /// <see cref="ArmoryLevelOne"/>, <see cref="ExitGates"/>, Opens Heavy Containment armories.
+    /// </summary>
+    ArmoryLevelTwo = 256, // 0x0100
+
+    /// <summary>
+    /// <see cref="ArmoryLevelTwo"/>, <see cref="Intercom"/>, Opens MicroHID room.
+    /// </summary>
+    ArmoryLevelThree = 512, // 0x0200
+
+    /// <summary>
+    /// <see cref="Checkpoints"/>.
+    /// </summary>
+    ScpOverride = 1024, // 0x0400
 }
